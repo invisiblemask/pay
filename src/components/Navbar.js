2dc1a6a6ac8b8e65984ce4d64783b203b1f69b90
Kelechi <okekekelechi96@gmail.com>
2022-08-19 11:41:47 +0100

    adding changes #3

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -16,14 +16,14 @@ const Navbar = () => {
                     <span className='bg-white text-black px-1 rounded-lg'>New</span> We are excited to announce our brand new product. <span className='underline cursor-pointer'>Learn More.</span>
                 </div>
                 <div className='flex justify-between p-4 lg:px-24'>
-                    <div className='lg:flex lg:items-center lg:justify-between lg:gap-20'>
+                    <div className='lg:flex lg:items-center lg:justify-between lg:gap-20 md:flex md:items-center md:justify-between md:gap-20'>
                         <div>
                             <a className='text-2xl text-cyan-500 font-bold' href='/'>
                                 Pay
                             </a>
                         </div>
 
-                        <div className='hidden lg:flex lg:gap-10 text-sm'>
+                        <div className='hidden lg:flex lg:gap-10 text-sm lg:text-sm md:flex md:gap-10 md:text-xs'>
                             <a
                                 href='/'
                                 className='flex flex-row items-center gap-1'
@@ -35,18 +35,18 @@ const Navbar = () => {
                             <a href='/'>Blog</a>
                         </div>
                     </div>
-                    <div className='hidden lg:flex lg:flex-row gap-4'>
-                        <button className='border text-cyan-600 font-bold rounded-3xl px-4 py-2 text-sm'>Sign In</button>
-                        <button className='border bg-cyan-600 text-gray-50 font-bold rounded-3xl px-4 py-2 text-sm'>Free trial</button>
+                    <div className='hidden lg:flex lg:flex-row gap-4 md:flex md:flex-row'>
+                        <button className='border text-cyan-600 font-bold rounded-3xl px-4 py-2 text-sm lg:text-sm lg:py-2 md:text-xs md:py-1'>Sign In</button>
+                        <button className='border bg-cyan-600 text-gray-50 font-bold rounded-3xl px-4 py-2 text-sm lg:text-sm lg:py-2 md:text-xs md:py-1'>Free trial</button>
                     </div>
                     <div
-                        className='text-xl lg:hidden cursor-pointer'
+                        className='text-xl lg:hidden md:hidden cursor-pointer'
                         onClick={handleNavClick}
                     >
                         <AiOutlineMenu />
                     </div>
                 </div>
-                <div className={showNav ? 'bg-white absolute right-0 top-0 shadow-xl h-screen z-50 w-3/4 lg:hidden' : 'hidden'}>
+                <div className={showNav ? 'bg-white absolute right-0 top-0 shadow-xl h-screen z-50 w-3/4 lg:hidden md:hidden' : 'hidden'}>
                     <div
                         className='text-xl float-right p-4 cursor-pointer'
                         onClick={handleNavClick}
